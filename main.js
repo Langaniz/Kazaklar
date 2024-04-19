67b07fbd709d5cd10a0c28901c14f4fa25ffa21e
Langaniz <149571902+Langaniz@users.noreply.github.com>
2024-04-19 09:29:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,61 +1,66 @@
-
+let page = 1;
 let img = document.createElement("img");
 img.classList.add("mainImg");
 
 const MAİN = document.querySelector(".MAİN");
 
+window.addEventListener('DOMContentLoaded', callProduct);
+window.addEventListener('scroll', checkIfNearBottom);
 
-
-
-
-
-const addHTML = async () => {
-  for (let i = 0; i <= 19; i++) {
-      const img = document.createElement("img");
-      img.classList.add("mainImg");
-
-      const mainDiv = document.createElement("div");
-      mainDiv.classList.add("main_Div");
-      mainDiv.classList.add("headline");
-
-      let imgDiv = document.createElement("div");
-      imgDiv.classList.add("img_Div");
-
-      const bottomDiv = document.createElement("div");
-      bottomDiv.classList.add("bottom_Div");
-
-      let ürnName = document.createElement("p");
-      ürnName.classList.add("ürn_Name");
-
-      let ürnFiyat = document.createElement("p");
-      ürnFiyat.classList.add("ürn_Fiyat");
-
-      let addBtn = document.createElement("button");
-      addBtn.classList.add("add_Btn");
-      addBtn.textContent = "Sepete Ekle";
-
-      imgDiv.appendChild(img);
-      mainDiv.appendChild(imgDiv);
-      bottomDiv.appendChild(ürnName);
-      bottomDiv.appendChild(ürnFiyat);
-      bottomDiv.appendChild(addBtn);
-      mainDiv.appendChild(bottomDiv);
-      MAİN.appendChild(mainDiv);
-
-      fetch("https://farktorapi.com/new/?company=Fr-5500191&category=47170")
-          .then(response => {
-              return response.json();
-          })
-          .then(data => {
-              let imgName = Array.isArray(data) ? data : [data];
-              ürnName.innerHTML = imgName[0].products[i].name;
-              ürnFiyat.innerHTML = imgName[0].products[i].priceMarket + " TL";
-
-              let imgSrc = Array.isArray(data) ? data : [data];
-              imgSrc = "https://images.farktorcdn.com/img/800x1200/Library/Upl/5500191/Product/" + imgSrc[0].products[i].photo;
-              img.src = imgSrc;
-          });
+function checkIfNearBottom() {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+    callProduct();
   }
-};
+}
 
-addHTML();
+//TODO---------------------------------------------------------
+
+function callProduct() {
+    fetch("https://farktorapi.com/new/?company=Fr-5500191&category=47170&page=" + page)
+      .then(response => response.json())
+      .then(data => {
+        addHTML(data?.products);
+        page++;
+        console.log(data)
+      })
+      .catch(error => {
+        console.error('Hata:', error);
+      });
+  }
+
+const addHTML = async (products) => {
+    products.map((perItem, photo)=>{
+        const img = document.createElement("img");
+        img.classList.add("mainImg");
+        img.src = "https://images.farktorcdn.com/img/800x1200/Library/Upl/5500191/Product/" + perItem.photo;
+        
+        const mainDiv = document.createElement("div");
+        mainDiv.classList.add("main_Div");
+  
+        let imgDiv = document.createElement("div");
+        imgDiv.classList.add("img_Div");
+  
+        const bottomDiv = document.createElement("div");
+        bottomDiv.classList.add("bottom_Div");
+  
+        let ürnName = document.createElement("p");
+        ürnName.classList.add("ürn_Name");
+        ürnName.textContent =perItem.name;
+  
+        let ürnFiyat = document.createElement("p");
+        ürnFiyat.classList.add("ürn_Fiyat");
+        ürnFiyat.innerHTML=perItem.priceSale + " TL";
+  
+        let addBtn = document.createElement("button");
+        addBtn.classList.add("add_Btn");
+        addBtn.textContent = "Sepete Ekle";
+  
+        imgDiv.appendChild(img);
+        mainDiv.appendChild(imgDiv);
+        bottomDiv.appendChild(ürnName);
+        bottomDiv.appendChild(ürnFiyat);
+        bottomDiv.appendChild(addBtn);
+        mainDiv.appendChild(bottomDiv);
+        MAİN.append(mainDiv);
+    })
+};
